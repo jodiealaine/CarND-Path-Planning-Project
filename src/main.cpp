ec2be37f47364ade8c0bd8f53dc354362a65ac57
jodiealaine <me@jodiealaine.com>
2017-08-17 21:50:00 +0100

    Slight adjustment to dist_inc to help reduce the average speed

--- src/main.cpp
+++ src/main.cpp
@@ -241,11 +241,16 @@ int main() {
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	
-            double dist_inc = 0.5;
+            double dist_inc = 0.3; // helps keep the average speed below the limit - previously tried 0.5
             for(int i = 0; i < 50; i++)
             {
-                  next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-                  next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+              // Use Frenet s & d for x and y points
+              double next_s = car_s + (i + 1) * dist_inc;
+              double next_d = 6; // middle lane 1.5 m from waypoint, thus 1.5 * 4
+              vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+              next_x_vals.push_back(xy[0]);
+              next_y_vals.push_back(xy[1]);
             }
 
             msgJson["next_x"] = next_x_vals;
